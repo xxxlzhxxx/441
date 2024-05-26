4925f0e4b6f9cea9d07c466bc1c83b114b3f1da9
kawasemii <bn_kiwami@outlook.com>
2024-05-26 19:41:04 +0800

    getchatts & postchatt api

--- lab1/lab1_backend/app/views.py
+++ lab1/lab1_backend/app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+def getchatts(request):
+    if request.method != 'GET':
+        return HttpResponse(status=404)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM chatts ORDER BY time DESC;')
+    rows = cursor.fetchall()
+
+    response = {}
+    response['chatts'] = rows
+    return JsonResponse(response)
+
+
+@csrf_exempt
+def postchatt(request):
+    if request.method != 'POST':
+        return HttpResponse(status=404)
+    json_data = json.loads(request.body)
+    username = json_data['username']
+    message = json_data['message']
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO chatts (username, message) VALUES '
+                   '(%s, %s);', (username, message))
+    return JsonResponse({})
--- lab1/lab1_backend/routing/urls.py
+++ lab1/lab1_backend/routing/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getchatts/', views.getchatts, name='getchatts'),
+    path('postchatt/', views.postchatt, name='postchatt'),
 ]
